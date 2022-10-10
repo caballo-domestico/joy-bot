6ff64d19b36766b0e0767b08a7c75b38e22ec9d5
ElisaVerza <velis.smile@gmail.com>
2022-10-10 19:19:19 +0200

    Merge Dynamo Terraform

--- demiurges/create-ec2-frontend/main.tf
+++ demiurges/create-ec2-frontend/main.tf
@@ -14,9 +14,39 @@ provider "aws" {
 }
 
 resource "aws_instance" "frontend" {
-  ami           = "ami-026b57f3c383c2eec"
-  instance_type = "t2.micro"
+  ami                    = "ami-026b57f3c383c2eec"
+  instance_type          = "t2.micro"
+  key_name               = "prova"
+  vpc_security_group_ids = [aws_security_group.example.id]
   tags = {
     Name = "frontend"
+  }
+}
+
+resource "aws_security_group" "example" {
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+  }
+}
+
+module "dynamodb_table" {
+  source = "terraform-aws-modules/dynamodb-table/aws"
+
+  name     = "analisi"
+  hash_key = "cf"
+
+  attributes = [
+    {
+      name = "cf"
+      type = "S"
+    }
+
+  ]
+
+  tags = {
+    Name = "user-interface"
   }
 }
--- frontend/webserver/app.py
+++ frontend/webserver/app.py
@@ -1,6 +1,32 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, request, url_for, flash, redirect
+import boto3
 
-@app.route('/')
-def hello_world():
-    return 'Hello, Docker!'
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'df0331cefc6c2b9a5d0208a726a5d1c0fd37324feba25509'
+messages = [{'title': 'Message One',
+             'content': 'Message One Content'},
+            {'title': 'Message Two',
+             'content': 'Message Two Content'}
+            ]
+
+@app.route('/', methods=('GET', 'POST'))
+def index():
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('analisi')
+
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        elif not content:
+            flash('Content is required!')
+        else:
+            table.put_item(
+            Item={
+                    'cf': title,
+                    'tipologia': content,
+                }
+            )
+    return render_template('index.html')
