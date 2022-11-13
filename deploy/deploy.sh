f284f7fb18dfce394c7dbcf7dd27adf5af18a18b
Torkin <32265040+Torkin1@users.noreply.github.com>
2022-11-13 19:43:20 +0100

    minor changes: - ansible hosts file is now truncated before every write from getIp.py - now you can skip terraform and docker steps in deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,7 +2,7 @@
 
 # This script is used to deploy the application to an AWS ec2 instance
 
-usage="Usage: $0 SSH_KEY_PATH "
+usage="Usage: $0 SSH_KEY_PATH [--skip-terraform]  [--skip-dependencies]"
 trap 'echo "\"${BASH_COMMAND}\" command failed with exit code $?."' ERR # who made the mess???
 
 if [ $# -lt 1 ]
@@ -11,29 +11,52 @@ then
     exit 1
 fi
 
+while [ "$1" != "" ]; do
+    case $1 in
+    --skip-terraform)
+        skipTerraform=true
+        ;;
+    --skip-dependencies)
+        skipDependencies=true
+        ;;
+    *)
+        export SSH_PATH=$1
+        ;;
+    esac
+    shift
+done
+
 scriptDir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 rootDir="$( cd -- "$(dirname "$scriptDir")" >/dev/null 2>&1 ; pwd -P )"
-export SSH_PATH=$1
 set -e
 
 # prepare infrastructure with terraform
-cd "$rootDir"/demiurges
-./launch.sh "create-ec2-frontend" "create-microservices-resources" --cfg-path ~/.aws/config --cdls-path ~/.aws/credentials
-terraform -chdir=create-ec2-frontend output -json > "$scriptDir/infrastructure.json"
+if [ -z "$skipTerraform" ]
+then
+    echo "$0": deploying infrastructure with terraform
+    cd "$rootDir"/demiurges
+    ./launch.sh "create-ec2-frontend" "create-microservices-resources" --cfg-path ~/.aws/config --cdls-path ~/.aws/credentials
+    terraform -chdir=create-ec2-frontend output -json > "$scriptDir/infrastructure.json"
+fi
 
 # compile protobuf and grpc files
-echo compiling protobuf and grpc stubs
+echo "$0": compiling protobuf and grpc stubs
 make -C "$rootDir"/frontend stubs
 
 # setup ansible hosts file
-echo setting up ansible hosts file
+echo "$0": setting up ansible hosts file
 cd "$scriptDir" 
 python3 getIp.py
 
 # prepare docker environment and install app with ansible
-echo deploying app to remote host with ansible
-export ANSIBLE_HOST_KEY_CHECKING=False
-ansible-playbook -v -i hosts -K docker.yml deployApp.yml
+echo "$0": deploying app to remote host with ansible
+#export ANSIBLE_HOST_KEY_CHECKING=False
+if [ -z "$skipDependencies" ]
+then
+    playbooks+=("docker.yml")
+fi
+playbooks+=("deployApp.yml")
+ansible-playbook -v -i hosts -K "${playbooks[@]}"
 
 set +e
 
